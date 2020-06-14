78c02befffdb39320260b0184a3ed7f82d07b6a7
Motohitro Yamagishi <motohiro993@gmail.com>
2020-06-14 13:44:10 +0900

    configuration routing

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,5 +1,9 @@
 class Admin::TopController < Admin::Base
   def index
-    render action: "index"
+    if current_adminstrator
+      render action: "dashboard"
+    else
+      render action:"index"
+    end
   end
 end
