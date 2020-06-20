607234ce356e12a9094bc0cbb6b71b9d5120e78c
Motohitro Yamagishi <motohiro993@gmail.com>
2020-06-20 16:55:14 +0900

    Storong Parameter

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,25 +3,29 @@ module ErrorHandlers
 
 
   included do
-
     rescue_from StandardError, with: :rescue500
-    rescue_from ApplicationContoroller::Forbidden, with: :rescue403
-    rescue_from ApplicationContoroller::IpAddressRejected, with: :rescue403
+    rescue_from ApplicationController::Forbidden, with: :rescue403r
+    rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
+    rescue_from ActionController::ParameterMissing, with: :rescue400
+  end
 
-    private def rescue403(e)
-      @exception = e
-      render "errors/forbidden", status: 403
-    end
+  private def rescue400(e)
+    render "errors/bad_request", status: 400
+  end
 
-    private def rescue404(e)
-      @exception = e
-      render "errors/not_found", status: 404
-    end
+  private def rescue403(e)
+    @exception = e
+    render "errors/forbidden", status: 403
+  end
 
-    private def rescue500(e)
-      render "errors/internal_server_error", status: 500
-    end
+  private def rescue404(e)
+    @exception = e
+    render "errors/not_found", status: 404
+  end
+
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
   end
 
 end
