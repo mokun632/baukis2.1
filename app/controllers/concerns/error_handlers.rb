e7818514a397e92b8ec1f3f1b854740a0fd549f3
Motohitro Yamagishi <motohiro993@gmail.com>
2020-06-27 14:07:08 +0900

    model associate

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -4,7 +4,7 @@ module ErrorHandlers
 
   included do
     rescue_from StandardError, with: :rescue500
-    rescue_from ApplicationController::Forbidden, with: :rescue403r
+    rescue_from ApplicationController::Forbidden, with: :rescue403
     rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
     rescue_from ActionController::ParameterMissing, with: :rescue400
--- app/models/staff_member.rb
+++ app/models/staff_member.rb
@@ -1,4 +1,6 @@
 class StaffMember < ApplicationRecord
+  has_many :events, class_name: "Staff_Event", dependent: :destroy
+
   def password=(raw_password)
     if raw_password.kind_of?(String)
       self.hashed_password = BCrypt::Password.create(raw_password)
