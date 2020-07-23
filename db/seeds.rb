826f07fa73b2e2a4bf59391d18fb31259740e107
mokun632 <motohiro993@gmail.com>
2020-07-23 20:24:03 +0900

    index show new edit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-table_name = %w(staff_members adminstrators staff_events)
+table_name = %w(staff_members adminstrators staff_events customers)
 
 table_name.each do |table_name|
   path = Rails.root.join("db", "seeds", Rails.env, "#{table_name}.rb")
